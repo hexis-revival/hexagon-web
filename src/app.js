97e284f6ccea29c080fdb93cd9b21b0882fc11df
Lekuru <contact@lekuru.xyz>
2024-10-08 22:19:15 +0200

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="app-logo" alt="logo" />
+        <p>
+          I can't believe it's not butter.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
