7c4baabc1e4c845e76ff31d92c9ca488c7bf0c21
Lekuru <contact@lekuru.xyz>
2024-10-08 22:24:33 +0200

    Refactor class names

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ import './app.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app">
+      <header className="app-header">
         <img src={logo} className="app-logo" alt="logo" />
         <p>
           I can't believe it's not butter.
